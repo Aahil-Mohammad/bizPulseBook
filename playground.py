9259421e2733b7946be86d02d90972db6452fc09
Aahil-Mohammad <96430442+Aahil-Mohammad@users.noreply.github.com>
2024-06-19 18:15:59 -0400

    Update playground.py

--- playground.py
+++ playground.py
@@ -3,16 +3,19 @@ import os
 from streamlit_supabase_auth import login_form, logout_button
 
 os.environ["SUPABASE_URL"] = "https://cweordholuvkdavqvzlz.supabase.co"
-os.environ["SUPABASE_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZW9yZGhvbHV2a2RhdnF2emx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMjc1NDIsImV4cCI6MjAzMzcwMzU0Mn0.K7NsU8WCDOwcONLabLCGp7o_RJl02Z4YPopH9okUiUQ"
+os.environ["SUPABASE_KEY"] = "your_supabase_key_here"
 
 session = login_form(
     url="https://cweordholuvkdavqvzlz.supabase.co",
-    apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZW9yZGhvbHV2a2RhdnF2emx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMjc1NDIsImV4cCI6MjAzMzcwMzU0Mn0.K7NsU8WCDOwcONLabLCGp7o_RJl02Z4YPopH9okUiUQ",
+    apiKey="your_supabase_api_key_here",
     providers=["google"]
 )
 
-# Get the value of the 'page' query parameter
-page = st.query_params["page"]
+# Check if 'page' exists in query params
+page = st.query_params.get("page", None)
+
+if page == "success":
+    st.write("Page is success!")
 
 with st.sidebar:
     logout_button()

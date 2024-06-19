c4394c5469db54959e705fdb2e4b9c25793fd5f0
Aahil-Mohammad <96430442+Aahil-Mohammad@users.noreply.github.com>
2024-06-19 17:57:00 -0400

    Update playground.py

--- playground.py
+++ playground.py
@@ -1,14 +1,18 @@
 import streamlit as st
+import os
+from streamlit_supabase_auth import login_form, logout_button
 
-from st_login_form import login_form
+os.environ["SUPABASE_URL"] = "https://cweordholuvkdavqvzlz.supabase.co"
+os.environ["SUPABASE_KEY"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZW9yZGhvbHV2a2RhdnF2emx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMjc1NDIsImV4cCI6MjAzMzcwMzU0Mn0.K7NsU8WCDOwcONLabLCGp7o_RJl02Z4YPopH9okUiUQ"
 
+session = login_form(
+    url="https://cweordholuvkdavqvzlz.supabase.co",
+    apiKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZW9yZGhvbHV2a2RhdnF2emx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMjc1NDIsImV4cCI6MjAzMzcwMzU0Mn0.K7NsU8WCDOwcONLabLCGp7o_RJl02Z4YPopH9okUiUQ",
+    providers=["google"]
+)
 
-client = login_form()
+# Get the value of the 'page' query parameter
+page = st.query_params["page"]
 
-if st.session_state["authenticated"]:
-    if st.session_state["username"]:
-        st.success(f"Welcome {st.session_state['username']}")
-    else:
-        st.success("Welcome guest")
-else:
-    st.error("Not authenticated")
+with st.sidebar:
+    logout_button()
